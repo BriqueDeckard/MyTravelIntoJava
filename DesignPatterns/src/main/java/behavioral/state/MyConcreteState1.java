8f3c3ec6e5f7a49e34b1d19604c9392c967c2190
Pierre ANTOINE <pantoine@apside-groupe.com>
2023-12-08 12:02:53 +0100

    #9

---
+++ DesignPatterns/src/main/java/behavioral/state/MyConcreteState1.java
@@ -0,0 +1,16 @@
+package behavioral.state;
+
+public class MyConcreteState1 implements MyState{
+	String name = "State 1";
+
+	public String getName() {
+		return name;
+	}
+
+
+	@Override
+	public void doAction(MyContext context) {
+		System.out.println("MyConcreteState1 state");
+		context.setState(this);
+	}
+}
