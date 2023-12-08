f6c4d1894e5f0f0285444b8670010151c327c5ac
Pierre ANTOINE <pantoine@apside-groupe.com>
2023-12-08 11:16:46 +0100

    #7

---
+++ DesignPatterns/src/main/java/behavioral/memento/MyMemento.java
@@ -0,0 +1,13 @@
+package behavioral.memento;
+
+public class MyMemento {
+	private String state;
+
+	public MyMemento(String state){
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+}
---
+++ DesignPatterns/src/main/java/behavioral/memento/MyMementoApp.java
@@ -0,0 +1,22 @@
+package behavioral.memento;
+
+public class MyMementoApp {
+	public static void main(String[] args) {
+		MyOriginator originator = new MyOriginator();
+		MyCareTaker myCareTaker = new MyCareTaker();
+		originator.setState("STATE_1");
+		myCareTaker.add(originator.saveStateToMemento());
+		originator.setState("STATE_2");
+		myCareTaker.add(originator.saveStateToMemento());
+		originator.setState("STATE_3");
+		myCareTaker.add(originator.saveStateToMemento());
+
+		System.out.println("Current state: " +originator.getState());
+		originator.getStateFromMemento(myCareTaker.get(0));
+		System.out.println("Previous state: " +originator.getState());
+		originator.getStateFromMemento(myCareTaker.get(1));
+		System.out.println("Previous state: " +originator.getState());
+		originator.getStateFromMemento(myCareTaker.get(2));
+		System.out.println("Previous state: " +originator.getState());
+	}
+}
