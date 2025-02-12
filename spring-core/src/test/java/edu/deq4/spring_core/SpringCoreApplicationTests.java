67b30ed99e32b189a2671d78562171981b92db95
Pierre Antoine <pierre.antoine@apside.com>
2025-02-12 16:07:08 +0100

    #61 - Spring Core

---
+++ spring-core/src/test/java/edu/deq4/spring_core/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.deq4.spring_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
